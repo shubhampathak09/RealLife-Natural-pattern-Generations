3f1248bc70e5a0b06d64e61bd702f4e40ce758ae
shubham pathak <95shubham1@gmail.com>
2021-03-03 20:46:29 +0530

    mitiosis some fix in code

--- mitosis-simulation/mitosis-simulation/cell.js
+++ mitosis-simulation/mitosis-simulation/cell.js
@@ -1,16 +1,13 @@
 
-function Cell(pos, r, c) {
-    if (pos) {
-      this.pos = pos.copy();
-    } else {
-      this.pos = createVector(random(width), random(height));
-    }
+function Cell(x,y, r, c) {
+    this.x=random(width);
+    this.y=random(height);
   
     this.r = r || 60;
     this.c = c || color(random(100, 255), 0, random(100, 255), 100);
   
     this.clicked = function(x, y) {
-      var d = dist(this.pos.x, this.pos.y, x, y);
+      var d = dist(this.x, this.y, x, y);
       if (d < this.r) {
         return true;
       } else {
@@ -20,18 +17,23 @@ function Cell(pos, r, c) {
   
     this.mitosis = function() {
       //this.pos.x += random(-this.r, this.r);
-      var cell = new Cell(this.pos, this.r * 0.8, this.c);
+      var cell = new Cell(this.x,this.y, this.r * 0.8, this.c);
       return cell;
     };
   
     this.move = function() {
       var vel = p5.Vector.random2D(); // crazy funtion, not working wasted my entire day fucccck.........
-      this.pos.add(vel);
+      var vx=random(-50,50);
+      var vy=random(-50,50);
+      this.x+=vx;
+      this.y+=vy;
     };
   
     this.show = function() {
       noStroke();
       fill(this.c);
-      ellipse(this.pos.x, this.pos.y, this.r, this.r);
+      ellipse(this.x, this.y, this.r, this.r);
     };
+
+    // fixed few things still need to fix plenty more stuff will come back in few months 3-3-2021
   }
